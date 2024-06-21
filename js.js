9894b254308e8e17400756c1a613a7a749f4eb2a
durgeshbhalkar <durgeshbhalkar@gmail.com>
2024-06-22 01:57:52 +0530

    Forms

---
+++ js.js
@@ -0,0 +1,6 @@
+/*let newButtton= document.createElement("Button");;
+newButtton.innerText="click me";
+newButtton.style.color="white";
+newButtton.style.backgroundColor="red";
+document.querySelector("body").prepend(newButtton);*/
+ 
